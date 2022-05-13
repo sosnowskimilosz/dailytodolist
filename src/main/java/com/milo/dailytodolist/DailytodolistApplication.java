5b9b7cdcf29eab75dc5418b4b7648244cd065176
sosnowskimilosz <sosnowskimilosz@gmail.com>
2022-05-13 15:38:28 +0200

    initial commit with files

---
+++ src/main/java/com/milo/dailytodolist/DailytodolistApplication.java
@@ -0,0 +1,13 @@
+package com.milo.dailytodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailytodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailytodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milo/dailytodolist/DailytodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milo.dailytodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailytodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
